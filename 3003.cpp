dcd2e0b6582870b1118464c2f0912ab8235db6c3
huiiiiseoooo <sshs1552@naver.com>
2023-02-07 14:18:58 +0900

    3003

--- 3003.cpp
+++ 3003.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
+using namespace std;
 int main(){
-    int king,queen;
-    
+    int i[]={1,1,2,2,2,8};
+    int j[6];
+    for(int k=0; k<6; k++){
+        cin>>j[k];
+    }
+    for(int k=0; k<6; k++){
+        if(i[k]==j[k]){
+            cout <<"0 ";
+        }
+        else if(i[k]<j[k]){
+            cout<<i[k]-j[k]<<" ";
+        }
+        else{
+            cout<<i[k]-j[k]<<" ";
+        }
+    }
 }
