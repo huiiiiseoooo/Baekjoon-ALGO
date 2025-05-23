8f5bf0598155dd2b2dc8259ffe8f9bb927ab9315
huiiiiseoooo <sshs1552@naver.com>
2025-05-23 15:45:46 +0900

    2751 푸는중

--- 2751/src/Main.java
+++ 2751/src/Main.java
@@ -3,10 +3,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer[] integer;
-       
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int left = 0, mid, right = arr.length - 1;
 
+
+    }
+
+    public static void merge(int[] arr, int left, int right){
+        if(left<right){
+            merge(arr, left, (left+right)/2);
+            merge(arr, (left+right)/2+1, right);
+            merge_sort(arr, left, right);
+        }
+    }
+
+    public static void merge_sort(int[] arr, int left, int right){
+        int i = left,mid = (left+right)/2, j = mid+1, k = left;
+        int[] temp = new int[arr.length];
+        while(i<left && j<right ){
+            if(arr[i]<arr[j]){
+                temp[k++] = arr[i++]
+            }
+            else{
+                temp[k++] = arr[j++];
+            }
+        }
+        if(i>mid){
+            for(int )
+        }
     }
 }
