0307fcd1a54fdba49cd5ef8a18662d72faa16a53
huiiiiseoooo <sshs1552@naver.com>
2025-05-28 22:12:19 +0900

    2751 문제풀이

--- 2751/src/Main.java
+++ 2751/src/Main.java
@@ -1,39 +1,63 @@
-
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    static int[] temp;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        int left = 0, mid, right = arr.length - 1;
+        temp = new int[n]; // temp 배열 재사용
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
 
+        mergeSort(arr, 0, n - 1);
+
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i < n; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
     }
 
-    public static void merge(int[] arr, int left, int right){
-        if(left<right){
-            merge(arr, left, (left+right)/2);
-            merge(arr, (left+right)/2+1, right);
-            merge_sort(arr, left, right);
+    public static void mergeSort(int[] arr, int left, int right) {
+        if(left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
         }
     }
 
-    public static void merge_sort(int[] arr, int left, int right){
-        int i = left,mid = (left+right)/2, j = mid+1, k = left;
-        int[] temp = new int[arr.length];
-        while(i<left && j<right ){
-            if(arr[i]<arr[j]){
-                temp[k++] = arr[i++]
-            }
-            else{
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while(i <= mid && j <= right) {
+            if(arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
                 temp[k++] = arr[j++];
             }
         }
-        if(i>mid){
-            for(int )
+
+        while(i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while(j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for(int t = left; t <= right; t++) {
+            arr[t] = temp[t];
         }
     }
 }
